3792efce27e8f7c90a6a0c8cff9878805403f14a
zuozhuo <zuoyizhuo@qq.com>
2014-09-19 11:36:17 +0800

    tttt

---
+++ gulpfile2.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zuozhuo on 9/19/14.
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+    console.log('gulp installed successful!!!!')
+});
+
